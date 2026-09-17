f25b0a06f1ba0ad240707eba12faededec3c9779
ALi Salama <alysaied2000@gmail.com>
2026-09-17 18:29:57 +0300

    Complete calculator

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// let firstNumber
-// let secondNumber
-// let op
-let btns = document.querySelectorAll("button.btn")
+let firstNumber = ''
+let secondNumber = ''
+let op = ''
+let currentInput = "0"
 
-btns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        console.log(btn.textContent)
-    })
-})
+const btns = document.querySelectorAll(".btn")
+const screenDisplay = document.querySelector(".screen")
+// const operators = document.querySelectorAll(".operator")
+// const equalOperator = document.querySelector(".equal")
+// const clear = document.querySelector(".clear")
 
 const add = (num1, num2) => { return num1 + num2 }
 
@@ -29,4 +29,60 @@ const operate = (a, b, op) => {
     }
 }
 
-// console.log(operate(2, 3, '+'))
+const updateDisplay = (digit) => {
+    if(currentInput === "0" || currentInput === firstNumber){
+        currentInput = digit
+    }else{
+        currentInput += digit
+    }
+    screenDisplay.value = currentInput
+}
+
+const handleNumber = (value) => {
+    updateDisplay(value)
+}
+
+const handleOperator = (value) => {
+    firstNumber = currentInput
+    op = value
+    currentInput = ""
+}
+
+const handleCalc = () => {
+    if(firstNumber === "" || op === "") return
+    secondNumber = currentInput
+
+    let result = operate(Number(firstNumber), Number(secondNumber), op)
+    screenDisplay.value = result
+    currentInput = String(result)
+    firstNumber = ""
+    op = ""
+}
+
+const handleClear = () => {
+    currentInput = "0"
+    firstNumber = ""
+    secondNumber = ""
+    op = ""
+    screenDisplay.value = "0"
+}
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const value = btn.textContent
+        const isOperator = btn.classList.contains("operator")
+        const isEqual = value === "="
+        const isClear = value === "C"
+
+        if(isClear){
+            handleClear()
+        } else if(isEqual){
+            handleCalc()
+        } else if(isOperator){
+            handleOperator(value)
+        }else{
+            handleNumber(value)
+        }
+
+    })
+})
